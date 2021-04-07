65d5bb4abbef4f756eeb6e1dbb89455ab8d5238f
Rachel Estilo <rachelestilo@me.com>
2021-04-06 19:07:00 -0500

    Project init commit
    Major init commit.  Bringing out the "big guns"!
    
    (NOTE: some files had to be ignored bc they were too big to be pushed 😩 )

---
+++ Helper files/labelCount.py
@@ -0,0 +1,28 @@
+from xml.dom import minidom
+import os
+
+directory = r'[*/DIRECTORY/TO/ALL/IMAGE/IMAGES/SOURCE/HERE*]'
+
+positive = 0
+negative = 0
+for filename in os.listdir(directory):
+    if filename.endswith(".xml"):
+        doc = os.path.join(directory, filename)
+        mydoc = minidom.parse(doc)
+        
+        items = mydoc.getElementsByTagName('name')
+        label = items[0].firstChild.data
+        images = mydoc.getElementsByTagName('filename')
+        image = images[0].firstChild.data
+        print(image, label)
+        
+        if label == 'OnPhone':
+            positive += 1
+        elif label == 'NotOnPhone':
+            negative += 1
+    else:
+        continue
+
+print()
+print('Postive count:\t', positive)
+print('Negative count:\t', negative)
